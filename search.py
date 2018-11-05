7a91a82d54a186c31f36783c033e4ec7ded44799
arch <aa>
2018-11-05 23:01:31 +0100

    working fix poping element, change break to continue add comments in polish

--- search.py
+++ search.py
@@ -6,36 +6,47 @@ from collections import OrderedDict
 class BFS:
     def __init__(self, start):
         self.game = Game(start)
-        self.visited = OrderedDict()
+        # Slownik -- hash mapa, zapamietujekolejnosc dodawania
+        self.frontier = OrderedDict()
+        # zbior by nie sprawdzac czy juz w nim. bo w zbiorze jeden element moze byc tylko raz
         self.explored = set()
 
     def search(self):
         # Check if first state is goal state
+        # Ta czesc tylko sprawdza pierwszy stan i rozwijaja go w inne stany
 
+        # SPRAWDZENIE
         if self.game.check_result(self.game):
             return ''
-        else:
-            first_state = State(self.game.frame, None, None, self.game.zero_position, self.game.available_moves(self.game.zero_position))
-            for move in self.game.available_moves(self.game.zero_position):
-                new_state = self.game.new_state(first_state, move)
-                self.visited[hash(new_state)] = new_state
 
-            while len(self.visited) != 0:
-                current_state = self.visited.popitem()[1]
-                if self.game.check_result(current_state):
-                    print('U win!')
+        # ROZWIJNIE
+        first_state = State(self.game.frame, None, None,
+                            self.game.zero_position,
+                            self.game.available_moves(self.game.zero_position))
+        # DLA MOZLIWEGO RUCHU TWORZYMY NOWY STAN
+        for move in self.game.available_moves(first_state.zero_position):
+
+            new_state = self.game.new_state(first_state, move)
+            if self.game.check_result(new_state):
+                return new_state.move
+            # Dodajemy to otwartych stanow
+            self.frontier[hash(new_state)] = new_state
+        del first_state
+        #  ____________________________________________________________________
+        while len(self.frontier) != 0:
+            # Pobieramy klucz pierwszego elementu
+            current_state = iter(self.frontier).__next__()
+            # Zdejmujemy ten obiekt
+            current_state = self.frontier.pop(current_state)
+
+            for move in self.game.available_moves(current_state.zero_position):
+                other_state = self.game.new_state(current_state, move)
+                if other_state in self.frontier or other_state in self.explored:
+                    continue
+                if self.game.check_result(other_state):
+                    print('Win Wiecej niz 1 iteracje')
                     return ''
                 else:
-                    self.explored.add(hash(current_state))
-                    for move in self.game.available_moves(current_state.zero_position):
-                        other_state = State(current_state.frame,
-                                            current_state, move,
-                                            current_state.zero_position,
-                                            self.game.available_moves(current_state.zero_position))
-                        if self.game.check_result(other_state):
-                            return ''
-                        else:
-                            self.visited[hash(current_state)] = current_state
-
-
+                    self.frontier[hash(other_state)] = other_state
+            self.explored.add(current_state)
             current_state = self.game
--- tests/Search_Test.py
+++ tests/Search_Test.py
@@ -2,7 +2,7 @@ import unittest
 from state import State
 from search import BFS
 import numpy as np
-
+from collections import OrderedDict
 
 class BFSTest(unittest.TestCase):
 
@@ -20,12 +20,24 @@ class BFSTest(unittest.TestCase):
             [4, 5, 0],
             [7, 8, 6]])
         bfs = BFS(state_array)
-        bfs.search()
+        self.assertEqual(bfs.search(), 'D')
+
+    def test_dict(self):
+        a = OrderedDict()
+        a['foo'] = 5
+        a['bar'] = 3
+        # print(a.popitem())
+        print(a)
+        for i in a:
+            print(i)
+
+        b = iter(a)
+        print(b.__next__())
 
     def test_search2(self):
         state_array = np.array([
-            [1, 2, 3],
-            [4, 0, 5],
-            [7, 8, 6]])
+            [6, 2, 3],
+            [1, 5, 0],
+            [4, 7, 8]])
         bfs = BFS(state_array)
         bfs.search()
